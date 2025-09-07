f243801a67ceacbc6d8ee651d69556541f2aba8c
Siddami <dharmmy621@gmail.com>
2025-09-07 12:30:57 +0100

    feat: worked on operators script

---
+++ week-1/srcipt.js
@@ -0,0 +1,26 @@
+//.1 Programming test
+
+let num1, num2, name;
+
+num1 = 5;
+num2 = 7;
+name = "Sid"
+
+//addition
+const result = num1 + num2;
+console.log(num1 + num2)
+
+//subtraction
+console.log(num1 - num2)
+
+//multiplication 
+console.log(num1 * num2)
+
+//division
+console.log(num1 / num2)
+
+//concatenantion
+console.log("Hello " + name + "," + " the result of " + num1 + " + " + num2 + " is " + result)
+
+//alternative
+console.log(`Hello ${name}, the result of ${num1} + ${num2} is ${result}`)
